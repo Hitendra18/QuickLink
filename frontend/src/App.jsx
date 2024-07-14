697a21df2bebedfc54a7ffb20fc01039ed5d6377
Hitendra <hitendraverma001@gmail.com>
2024-07-14 18:10:39 +0530

    remove deleted file import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 
 // import "./App.css";
 import UploadPage from "./pages/UploadPage";
-import TestPage from "./pages/TestPage";
 import DownloadPage from "./pages/DownloadPage";
 import GitHub from "./components/GitHub";
 
@@ -12,7 +11,6 @@ function App() {
       <Routes>
         <Route index path="/" element={<UploadPage />} />
         <Route index path="/download/:fileId" element={<DownloadPage />} />
-        <Route index path="/test" element={<TestPage />} />
       </Routes>
       <GitHub />
     </div>
